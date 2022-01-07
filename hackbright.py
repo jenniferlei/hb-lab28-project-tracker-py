a8df02c6bb5a131608173b042e9c5201d9d3e4f6
Jennifer Lei <jenniferlei1928@gmail.com>
2022-01-07 04:39:45 +0000

    Updated handle_input function on hackbright.py

--- hackbright.py
+++ hackbright.py
@@ -178,6 +178,18 @@ def handle_input():
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
 
+        elif command == "project_by_title":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "grade_by_github_and_title":
+            github, title = args
+            get_grade_by_github_title(github, title)
+
+        elif command == "assign_grade":
+            github, title, grade = args
+            assign_grade(github, title, grade)
+
         else:
             if command != "quit":
                 print("Invalid Entry. Try again.")
@@ -186,7 +198,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    # handle_input()
+    handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
